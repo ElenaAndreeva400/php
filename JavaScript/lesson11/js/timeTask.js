9ad45e8c54fd3af7cd8002520ea53be8f2d53bab
ElenaAndreeva400 <56034664+ElenaAndreeva400@users.noreply.github.com>
2019-12-08 18:43:28 +0300

    Delete timeTask.js

--- JavaScript/lesson11/js/timeTask.js
+++
@@ -1,7 +0,0 @@
-
-let dateFromMoscow = moment('2019-11-17 17:30')// дата вылета
-console.log(dateFromMoscow.format("LLL"));
-dateFromMoscow.add(20, 'hours'); // время в полете Мск-Сидней 20 часов
-console.log(dateFromMoscow.format("LLL"));
-let arrivalTimeToSydney = dateFromMoscow.clone().tz('Australia/Sydney');
-console.log(arrivalTimeToSydney.format("LLL")); // разница +8 часов
