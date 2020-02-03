e12244b0c309262bc85a2396964b5db82e05191a
Elenandreeva <confeta@bk.ru>
2020-02-03 23:35:47 +0300

    6.1 и 7.1

--- JavaScript/lesson6/js/style.js
+++ JavaScript/lesson6/js/style.js
@@ -24,30 +24,66 @@ let articles = [
         author: "Виталий"
     }
 ];
-function createTable(arr, parentElement) {
-let table = document.getElementsByTagName("body")[0]
-.appendChild(document.createElement("table"));
-    // создаем пустую ячейку в верхнем левом углу таблицы
-let row = table.insertRow(0);
-let cell = row.insertCell(0);
-let pos = -1;
 
-    //создаем наименования столбцов
-for(let name in arr[0]) {
-     name = name.toUpperCase();
-        pos += 1;
-        cell = row.insertCell(pos);
-        cell.innerText = name;
-      };
-      // создаем ряды по количеству объектов и заполняем их значениями
-    for (let obj=0; obj<arr.length; obj++ ) {
-      let tr = table.insertRow();
-      for (let prop in arr[obj]) {
-        let td = tr.insertCell();
-        let cellTd = td.appendChild(document.createElement("cell"));
-        cellTd.innerText = arr[obj][prop];
+const goods = [
+  {
+      title: "Пианино",
+      price: 3000,
+      count: 25
+  },
+  {
+      title: "Гитара",
+      price: 1200,
+      count: 40
+  },
+  {
+      title: "Барабаны",
+      price: 2700,
+      count: 12
+  },
+  {
+      title: "Флейта",
+      price: 900,
+      count: 50
+  },
+  {
+      title: "Арфа",
+      price: 3400,
+      count: 5
+  }
+];
+
+function createTable(array) {
+      let table = document.createElement("table");
+      let firstObjectLength = Object.keys(array[0]).length;
+      let row = table.insertRow();
+      for (let title = 0; title < firstObjectLength; title++) {
+          let cell = row.insertCell();
+          cell.innerHTML = Object.keys(array[0])[title].toUpperCase();
+  }
+
+  for (let i = 0; i < array.length; i++) {
+      let row = table.insertRow();
+      console.log(row);
+      for (let j = 0; j < firstObjectLength; j++) {
+          let cell = row.insertCell();
+          console.log(cell);
+      cell.innerHTML = Object.values(array[i])[j];
+          
      }
-   }
- };
- let section = document.querySelector(".some_array_of_objects");
- createTable(articles, section);
+  }
+
+  document.body.append(table);
+
+  table.rows[0].addEventListener('click', sorting);
+
+  function sorting(event) {
+        let elements = event.target.innerHTML.toLowerCase();
+        let arr =  array.sort((a, b) => a[elements] > b[elements] ? 1 : a[elements] < b[elements] ? -1 : 0);
+        table.remove();
+        createTable(arr);
+  }
+}
+
+createTable(articles);
+//createTable(goods);
